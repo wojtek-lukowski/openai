0ec12755b9a4f9c7de41d561553507f036e6126e
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2025-01-14 18:23:37 +0100

    ip test

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import * as dotenv from 'dotenv';
 import scrapPortfolio from './scrapPortfolio.js';
 import db from "./database.js";
 import natural from "natural";
+import axios from 'axios';
+import { stringify } from 'querystring';
+// import news from "./news.js"
 
 dotenv.config();
 const app = express();
@@ -22,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://127.0.0.1:3000', 'https://lukowski.io']
-// allowedOrigins = ['*']
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
@@ -42,13 +44,13 @@ app.post('/query', async (req, res) => {
       const dbQuestionId = await processQuery(req.body.query);
       if (!dbQuestionId) {
         addData({question: req.body.query, answer: response});
-        console.log('added to db')
+        // console.log('added to db')
         // console.log('existing question', dbQuestionId)
         // const query = await getById(dbQuestionId);
         // res.send(query.data().answer)
         // console.log(query.id, '=>', (query.data()));
       } else {
-        console.log('NOT added to db')
+        // console.log('NOT added to db')
       }
     } catch {
        res.status(500).send({ error: 'Internal server error' })
@@ -131,7 +133,7 @@ const processQuery = async (query) => {
 
   similarities.sort((a,b) => b.similarity - a.similarity);
 
-  console.log(query, 'similarity', similarities[0].similarity)
+  // console.log(query, 'similarity', similarities[0].similarity)
   if (similarities[0].similarity > .9) {
     return similarities[0].questionId;
   } else return null;
@@ -149,6 +151,86 @@ const cosineSimilarity = (vec1, vec2) => {
   return dotProduct / (magnitude1 * magnitude2);
 };
 
+
+  app.get('/news', async (req, res) => {
+
+    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // const clientIP = '87.178.45.172'; //germany
+    // const clientIP = '89.178.45.172'; //russia
+    // const clientIP = '185.61.158.61'; //uk
+    // const clientIP = '91.239.6.243'; //albania
+
+    console.log(clientIP);
+    let ipData;
+
+    if (clientIP) {
+
+      try {
+        ipData = await getClientCountry(clientIP);
+        console.log(ipData);
+        res.send({ 
+          clientIP, 
+          country: ipData.country,
+          countryCode: ipData.countryCode,
+          city: ipData.city
+        })
+
+        if (clientIP && ipData.country && ipData.countryCode && ipData.city) {
+          saveIpData(clientIP, ipData);
+        }
+
+      } catch (error) {
+        res(500).send('Error')
+        console.log(error);
+      }
+
+      let clientCountryCode;
+
+      if (ipData.countryCode) {
+        clientCountryCode = ipData.countryCode.toLowerCase();
+      } else {
+        clientCountryCode = 'de';
+      }
+    } else {
+      clientCountryCode = 'de';
+    }
+
+    //   try {
+    //     const response = await axios.get("https://newsdata.io/api/1/latest", {
+    //       params: {
+    //         apikey: NEWS_API_KEY,
+    //         category: 'politics',
+    //         country: clientCountryCode
+    //       }
+    //     });
+
+    //     console.log(response.data.results.map(news => news.title));
+    //     res.send(response.data);
+    //   } catch {
+    //     res.status(500).send({ error: 'News error' })
+    // }
+  })
+
+  const saveIpData = async (ip, ipData) => {
+    try {
+      await db.collection('ip').add({ ip: ip, country: ipData.country, countryCode: ipData.countryCode,  city: ipData.city});
+    } catch (error) {
+      console.error("Error adding ip:", error);
+    }
+  };
+
+  const getClientCountry = async (clientIP) => {
+    try {
+      const response = await axios.get(`http://ip-api.com/json/${clientIP}`);
+
+      console.log(response.data)
+      const { country, countryCode, city } = response.data;
+      return { country, countryCode, city }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
